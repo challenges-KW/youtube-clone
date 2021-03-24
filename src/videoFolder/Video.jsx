1e5e09ba6eb4c5f4e0bf829082328f468e9a2599
katarzyna_w <katarzyna.koch@gmail.com>
2021-03-24 12:30:13 -0500

    added Video.test.js

--- src/videoFolder/Video.jsx
+++ src/videoFolder/Video.jsx
@@ -3,17 +3,17 @@ import './Video.css'
 export const Video = (props) => {
   return (
     <div className='videoWrapper' data-testid='videoComp'>
-        <div className='theVideo'>
+        <div className='theVideo' data-testid='videoClip'>
             {props.video}
         </div>
         <div className='videoText'>
-            <h4 className='name'>
+            <h4 className='name' data-testid='videoName'>
               {props.name}
             </h4>
-            <p className='publisher'>
+            <p className='publisher' data-testid='videoPublisher'>
               {props.publisher}
             </p>
-            <p className='viewsDate'>
+            <p className='viewsDate' data-testid='videoViewsDate'>
               {props.views} views - {props.date} years ago
             </p>
         </div>
---
+++ src/videoFolder/Video.test.js
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react';
+import { Video } from './Video'
+
+test('renders container', () => {
+  render(<Video />);
+  const videoContainer = screen.getByTestId('videoComp');
+  expect(videoContainer).toBeInTheDocument();
+});
+
+test('renders video', () => {
+    render(<Video />);
+    const videoClip = screen.getByTestId('videoClip');
+    expect(videoClip).toBeInTheDocument();
+  });
+
+  test('renders video name', () => {
+    render(<Video />);
+    const videoName = screen.getByTestId('videoName');
+    expect(videoName).toBeInTheDocument();
+  });
+
+  test('renders video publisher', () => {
+    render(<Video />);
+    const videoPublisher = screen.getByTestId('videoPublisher');
+    expect(videoPublisher).toBeInTheDocument();
+  });
+
+  test('renders video views and date', () => {
+    render(<Video />);
+    const videoViewsDate = screen.getByTestId('videoViewsDate');
+    expect(videoViewsDate).toBeInTheDocument();
+  });
