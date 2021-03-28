e8d3109b2075abfb0ae50206c6502e7e4be7e346
katarzyna_w <katarzyna.koch@gmail.com>
2021-03-28 17:56:43 -0500

    install material-ui/core and material-ui/icons plus add add imports and icons to mimic youtube

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 import { Video } from './videoFolder/Video.jsx'
 import { VideoList } from './videoList/VideoList.jsx'
 import {videoPlaylist} from './data'
+import MenuIcon from '@material-ui/icons/Menu'
+import SearchIcon from '@material-ui/icons/Search'
+import VideoCallIcon from '@material-ui/icons/VideoCall'
+import AppsIcon from '@material-ui/icons/Apps'
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
 function App() {
     //get the data
@@ -11,8 +16,19 @@ function App() {
   return (
     <div className="app">
       <div className="app-header-div">
-        <header className="app-header" data-testid='header'>
-          this is a youTube clone
+        <header className="app-header"  
+          data-testid='header'>
+            <MenuIcon />
+            <img 
+              classname='app-logo'
+              src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/640px-Logo_of_YouTube_%282015-2017%29.svg.png' 
+              alt='YouTube logo' 
+            />
+            <input type="text" />
+            <SearchIcon />
+            <VideoCallIcon />
+            <AppsIcon />
+            <NotificationsIcon />
         </header>
       </div>
       <div className="app-content">
