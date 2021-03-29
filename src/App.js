8f46ba84ce2f55628d6604d6cf676e06783efab4
katarzyna_w <katarzyna.koch@gmail.com>
2021-03-28 19:02:46 -0500

    move header to its own component and render in App.js with import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import { Video } from './videoFolder/Video.jsx'
-import { VideoList } from './videoList/VideoList.jsx'
+import Header from './header/Header'
+import { Video } from './videoFolder/Video'
+import { VideoList } from './videoList/VideoList'
 import {videoPlaylist} from './data'
-import MenuIcon from '@material-ui/icons/Menu'
-import SearchIcon from '@material-ui/icons/Search'
-import VideoCallIcon from '@material-ui/icons/VideoCall'
-import AppsIcon from '@material-ui/icons/Apps'
-import NotificationsIcon from '@material-ui/icons/Notifications';
+
 
 function App() {
     //get the data
@@ -15,28 +12,7 @@ function App() {
   //pass the data to its children as props
   return (
     <div className='app'>
-      <div className='app-header' data-testid='header'>
-        <div className='app-header-left'>
-          <MenuIcon className='app-header-icon' />
-          <img 
-            className='app-logo'
-            src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/1280px-YouTube_Logo_2017.svg.png' 
-            alt='YouTube logo' 
-          />
-        </div>
-        <div className='app-header-middle'>
-          <input 
-            type='text'
-            placeholder='Search'
-          />
-          <SearchIcon className='app-inputButton' />
-        </div>
-        <div className='app-header-right'>
-          <VideoCallIcon className='app-header-icon'/>
-          <AppsIcon className='app-header-icon'/>
-          <NotificationsIcon className='app-header-icon'/>
-        </div>
-      </div>
+      <Header />
       <div className="app-content">
       <Video vid={videoPlaylist[0]} />
       <VideoList vidList={videoPlaylist}/>
---
+++ src/header/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import MenuIcon from '@material-ui/icons/Menu'
+import SearchIcon from '@material-ui/icons/Search'
+import VideoCallIcon from '@material-ui/icons/VideoCall'
+import AppsIcon from '@material-ui/icons/Apps'
+import NotificationsIcon from '@material-ui/icons/Notifications';
+
+
+
+export default function Header() {
+    return (
+        <div className='app-header' data-testid='header'>
+        <div className='app-header-left'>
+          <MenuIcon className='app-header-icon' />
+          <img 
+            className='app-logo'
+            src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/1280px-YouTube_Logo_2017.svg.png' 
+            alt='YouTube logo' 
+          />
+        </div>
+        <div className='app-header-middle'>
+          <input 
+            type='text'
+            placeholder='Search'
+          />
+          <SearchIcon className='app-inputButton' />
+        </div>
+        <div className='app-header-right'>
+          <VideoCallIcon className='app-header-icon'/>
+          <AppsIcon className='app-header-icon'/>
+          <NotificationsIcon className='app-header-icon'/>
+        </div>
+      </div>
+    )
+}
