a8e9f05685ae765aee76e747591e8cbc7723bcbb
katarzyna_w <katarzyna.koch@gmail.com>
2021-03-31 13:09:47 -0500

    replace 2.png

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const videoPlaylist = [
     {
       id: 0,
       video: <iframe width="168" height="94.75" src="https://www.youtube.com/embed/PeonBmeFR8o" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>,
-      image: <img src='https://raw.githubusercontent.com/challenges-KW/youtube-clone/main/public/0.jpg' />,
+      image: <img src='https://raw.githubusercontent.com/challenges-KW/youtube-clone/main/public/0.png' />,
       name: 'Beyonce - Hold Up',
       publisher: 'Beyonce',
       views: '200M',
@@ -12,7 +12,7 @@ export const videoPlaylist = [
     {
       id: 1,
       video: <iframe width="168" height="94.75" src="https://www.youtube.com/embed/IGrV5mvlY8w" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>,
-      image: <img src='https://raw.githubusercontent.com/challenges-KW/youtube-clone/main/public/1.jpg' alt='Overprotected video screenshot'/>,
+      image: <img src='https://raw.githubusercontent.com/challenges-KW/youtube-clone/main/public/1.png' alt='Overprotected video screenshot'/>,
       name: 'Overprotected',
       publisher: 'Britney Spears',
       views: '741M',
@@ -22,7 +22,7 @@ export const videoPlaylist = [
     {
       id: 2,
       video: <iframe width="168" height="94.75" src="https://www.youtube.com/embed/m3-hY-hlhBg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>,
-      image: <img src='https://raw.githubusercontent.com/challenges-KW/youtube-clone/main/public/2.jpg' alt='How Will I Know video screenshot'/>,
+      image: <img src='https://raw.githubusercontent.com/challenges-KW/youtube-clone/main/public/2.png' alt='How Will I Know video screenshot'/>,
       name: 'How Will I Know',
       publisher: 'Whitney Houston',
       views: '851M',
@@ -32,7 +32,7 @@ export const videoPlaylist = [
     {
       id: 3,
       video: <iframe width="168" height="94.75" src="https://www.youtube.com/embed/LfRNRymrv9k" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>,
-      image: <img src='https://raw.githubusercontent.com/challenges-KW/youtube-clone/main/public/2.jpg' alt='Always Be My Baby video screenshot' />,
+      image: <img src='https://raw.githubusercontent.com/challenges-KW/youtube-clone/main/public/3.png' alt='Always Be My Baby video screenshot' />,
       name: 'Always Be My Baby',
       publisher: 'Mariah Carey',
       views: '900M',
@@ -42,7 +42,7 @@ export const videoPlaylist = [
     {
       id: 4,
       video: <iframe width="168" height="94.75" src="https://www.youtube.com/embed/tg00YEETFzg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>,
-      image: <img src='https://raw.githubusercontent.com/challenges-KW/youtube-clone/main/public/4.jpg' alt='We Found Love video screenshot' />,
+      image: <img src='https://raw.githubusercontent.com/challenges-KW/youtube-clone/main/public/4.png' alt='We Found Love video screenshot' />,
       name: 'We Found Love',
       publisher: 'Rihanna',
       views: '960M',
