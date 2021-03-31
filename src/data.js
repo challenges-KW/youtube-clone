e17e87836e3df20301e4a992cb69d345e7277e02
katarzyna_w <katarzyna.koch@gmail.com>
2021-03-31 13:05:01 -0500

    png images to public folder

--- src/data.js
+++ src/data.js
@@ -2,41 +2,51 @@ export const videoPlaylist = [
     {
       id: 0,
       video: <iframe width="168" height="94.75" src="https://www.youtube.com/embed/PeonBmeFR8o" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>,
+      image: <img src='https://raw.githubusercontent.com/challenges-KW/youtube-clone/main/public/0.jpg' />,
       name: 'Beyonce - Hold Up',
       publisher: 'Beyonce',
       views: '200M',
-      date: 'Jan 29, 2019'
+      date: 'Jan 29, 2019',
+      relativeDate: '2'
   },
     {
       id: 1,
       video: <iframe width="168" height="94.75" src="https://www.youtube.com/embed/IGrV5mvlY8w" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>,
+      image: <img src='https://raw.githubusercontent.com/challenges-KW/youtube-clone/main/public/1.jpg' alt='Overprotected video screenshot'/>,
       name: 'Overprotected',
       publisher: 'Britney Spears',
       views: '741M',
-      date: 11
+      date: 'July 28, 2003',
+      relativeDate: '18'
     }, 
     {
       id: 2,
       video: <iframe width="168" height="94.75" src="https://www.youtube.com/embed/m3-hY-hlhBg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>,
+      image: <img src='https://raw.githubusercontent.com/challenges-KW/youtube-clone/main/public/2.jpg' alt='How Will I Know video screenshot'/>,
       name: 'How Will I Know',
       publisher: 'Whitney Houston',
       views: '851M',
-      date: 10
+      date: 'November, 22, 1985',
+      relativeDate: '35'
     }, 
     {
       id: 3,
       video: <iframe width="168" height="94.75" src="https://www.youtube.com/embed/LfRNRymrv9k" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>,
+      image: <img src='https://raw.githubusercontent.com/challenges-KW/youtube-clone/main/public/2.jpg' alt='Always Be My Baby video screenshot' />,
       name: 'Always Be My Baby',
       publisher: 'Mariah Carey',
       views: '900M',
-      date: 9
+      date: 'March 9, 1996',
+      relativeDate: '25'
     }, 
     {
       id: 4,
       video: <iframe width="168" height="94.75" src="https://www.youtube.com/embed/tg00YEETFzg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>,
+      image: <img src='https://raw.githubusercontent.com/challenges-KW/youtube-clone/main/public/4.jpg' alt='We Found Love video screenshot' />,
       name: 'We Found Love',
       publisher: 'Rihanna',
       views: '960M',
-      date: 8
+      date: 'September, 22, 2011',
+      relativeDate: '10'
     }, 
   ]
--- src/videoList/videoList.jsx
+++ src/videoList/videoList.jsx
@@ -1,28 +1,57 @@
 import '../App'
 import React, { useState } from 'react'
-import { Video } from '../videoFolder/Video'
 import { videoPlaylist } from '../data'
 import './VideoList.css';
 
 
 export const VideoList = ( {vidList} ) => {
+    const [state, setstate] = useState({
+        date: '',
+        id: '',
+        image: '',
+        name: '',
+        publisher: '',
+        relativeDate: '',
+        video: '',
+        views: ''
+    })
+  
+    const changeState = (e) => {  
+        e.preventDefault()
+        console.log('this is e',  e.target)
+        setstate({
+            date: e.target.value,
+            id: e.target.value,
+            image: e.target.value,
+            name: e.target.value,
+            publisher: e.target.value,
+            relativeDate: e.target.value,
+            video: e.target.value,
+            views: e.target.value
+        }); 
+       }; 
+  
 
-    const [playSelected, setPlaySelected] = useState(false);
-
-    const onVideoClick = () => {
-        
-    };
 
     return (
         <div className ='videoListWrapper' data-testid='videoListWrapper'>
             <div className ='videoListVideo'>
                <div>
                    {vidList.map((video, index) => {
-                   return <Video 
-                        key={index}
-                        vid={video}
-                        onClick={onVideoClick}
-                    />
+                   return <div onClick={changeState} > 
+                                <div className='videoImage'>
+                                    {video.image}
+                                </div>
+                                <div className='videoName'>
+                                    {video.name}
+                                </div>
+                                <div className='videoPublisher'>
+                                    {video.publisher}
+                                </div>
+                                <div className='videoViews'>
+                                {video.views} views • {video.relativeDate} years ago
+                                </div>
+                            </div>
                     })}
                 </div>
             </div>
