984da3704a73684e2b76d2cf803beaccc51effb5
katarzyna_w <katarzyna.koch@gmail.com>
2021-04-02 12:09:04 -0500

    deconstructed props in SingleVideo.js

--- src/videoList/SingleVideo.js
+++ src/videoList/SingleVideo.js
@@ -2,21 +2,21 @@ import React from 'react'
 import './VideoList.css';
 import MusicIcon from '@material-ui/icons/MusicNote'
 
-export const SingleVideo = ( props ) => {
+export const SingleVideo = ( { video } ) => {
   return (
     <div className='singleVideoSnippet'>
       <div className='videoImage' data-testid='videoClip'>
-        {props.video.image}
+        {video.image}
       </div>
       <div className='videolistText'>
         <p className='videoName' data-testid='videoName'>
-          {props.video.name}
+          {video.name}
         </p>
         <p className='videoPublisher' data-testid='videoName'>
-          {props.video.publisher} <MusicIcon className='icons'/>
+          {video.publisher} <MusicIcon className='icons'/>
         </p>
         <p className='videoViews' data-testid='videoViews'>
-              {props.video.views} views • {props.video.relativeDate} years ago
+              {video.views} views • {video.relativeDate} years ago
             </p>
         </div>
     </div>
