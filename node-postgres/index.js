1aad90513510adabdfd3ab773aaa21decb3f9424
katarzyna_w <katarzyna.koch@gmail.com>
2021-04-08 19:06:15 -0500

    postgres database information in node-postgres/index

--- node-postgres/index.js
+++ node-postgres/index.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3001
 
+app.use(express.json())
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+    next();
+  });
+
 app.get('/', (req, res) => {
-    res.status(200).send('hello world!');
+    videonames.getVideoNames()
+    videodata.getVideoData()
+    res.send(videonames);
 })
 
 app.listen(port, () => {
