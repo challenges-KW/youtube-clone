580a96bf1d1415fe20b19d8ae8538bfd6beed3b0
katarzyna_w <katarzyna.koch@gmail.com>
2021-04-10 18:03:13 -0500

    videodata insert data into table

--- server/index.js
+++ server/index.js
@@ -19,14 +19,14 @@ app.get('/videos', function (req, res) {
 //create tables
 const queryString = 'CREATE TABLE IF NOT EXISTS videodata (videoId VARCHAR, imageId VARCHAR, views VARCHAR, date VARCHAR, relativeDate VARCHAR);'
 //insert data -- do with a loop
-const queryString2 = `INSERT INTO videonames(videoId, name, publisher) values('tg00YEETFzg', 'We Found Love', 'Rihanna');`
+const queryString2 = `INSERT INTO videodata(videoId, imageId, views, date, relativeDate) values('tg00YEETFzg', '4.png', '960M', 'September 22, 2011', '10');`
 //get data
 const queryString3 ='select * from videonames'
 //delete tables
 const queryString4 = 'drop table videodata'
 
 app.get('/api', (req, resp) => {
-  client.query(queryString, (err, res) => {
+  client.query(queryString2, (err, res) => {
     if (err) throw err
     resp.send(res)
     client.end()
