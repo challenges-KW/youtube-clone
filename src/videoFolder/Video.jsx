bfda866d216a95729a51aaedf4d5d05d876719e0
katarzyna_w <katarzyna.koch@gmail.com>
2021-04-16 21:16:28 -0500

    add thumbs up icon to Video.jsx

--- src/videoFolder/Video.jsx
+++ src/videoFolder/Video.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Video.css'
+import ThumbUpIcon from '@material-ui/icons/ThumbUp'
 
 export const Video = ({ vid }) => {
   
@@ -7,15 +8,16 @@ export const Video = ({ vid }) => {
     <div  
       className='videoWrapper'>
       <div className='theVideo' data-testid='videoClip'>
-      <iframe width="560" height="315" src={`https://www.youtube.com/embed/${vid.videoid}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        <iframe width="560" height="315" src={`https://www.youtube.com/embed/${vid.videoid}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       </div>
       <div className='videoText'>
         <p className='name' data-testid='videoName'>
           {vid.name}
         </p>
-        <p className='viewsDate' data-testid='videoViewsDate'>
-              {vid.views} views • {vid.date}
-            </p>
+        <div className='viewsDate' data-testid='videoViewsDate'>
+            {vid.views} views • {vid.date}
+            <ThumbUpIcon />
+        </div>
         </div>
       <hr></hr>
     </div>
